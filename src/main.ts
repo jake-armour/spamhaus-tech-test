1e349b87fd0b2f718085555c0b1d89666afa636b
Jake Armour <github@jakearmour.com>
2023-07-20 17:58:29 +0100

    chore: add icon font and vuetify labs features

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,20 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { VSkeletonLoader } from "vuetify/labs/VSkeletonLoader";
+import { VDataTable } from "vuetify/labs/VDataTable";
+import "@mdi/font/css/materialdesignicons.css";
 
 import { createPinia } from "pinia";
 
 import router from "@/router";
 
 const vuetify = createVuetify({
-  components,
+  components: { ...components, VSkeletonLoader, VDataTable },
   directives,
+  icons: {
+    defaultSet: "mdi",
+  },
 });
 
 const pinia = createPinia();
