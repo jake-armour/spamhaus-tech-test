a2df95331e4b014d494c7eb7f329eba6d369be0f
Jake Armour <github@jakearmour.com>
2023-07-20 19:01:38 +0100

    feat(Users): add new users components to index

---
+++ src/pages/users/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-if="usersData && !loading">
+      <UsersTableControls />
+      <UsersTable :data="usersData" />
+    </div>
+    <VSkeletonLoader v-else boilerplate loading class="mx-auto w-100" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { UseUserStore } from "@/state/stores/UserStore";
+import { storeToRefs } from "pinia";
+import UsersTableControls from "./UsersTableControls.vue";
+import UsersTable from "./UsersTable.vue";
+
+const { getUsersData: usersData, getUsersLoading: loading } = storeToRefs(
+  UseUserStore()
+);
+</script>
