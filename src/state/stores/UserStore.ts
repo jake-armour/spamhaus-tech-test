cfbf446122872e4ccf291f2c62edc7a0ea3e4ec1
Jake Armour <github@jakearmour.com>
2023-07-20 18:59:43 +0100

    feat(stores/UserStore): added user store
    returns data, loading boolean and a function to refresh data

---
+++ src/state/stores/UserStore.ts
@@ -0,0 +1,33 @@
+import { IUserData } from "@/typings/pages/UserTypes";
+import { ComputedRef, computed, ref, watch } from "vue";
+import { defineStore } from "pinia";
+import { useBaseUrl } from "@/helpers/UrlBuilder";
+import { useAsyncData } from "@/helpers/DataFetching";
+
+function refineData<T extends IUserData>(data: T[]): ComputedRef {
+  return computed(() => {
+    return data;
+  });
+}
+
+export const UseUserStore = defineStore("user", () => {
+  const apiUrl = useBaseUrl("users");
+  const { data, loading, refreshData } = useAsyncData<IUserData[]>(apiUrl);
+  const tempData = ref<IUserData[]>([]);
+
+  const getUsersData = computed(() => {
+    if (data.value) {
+      return refineData(data.value).value;
+    }
+    return [];
+  });
+  const getUsersLoading = computed(() => loading.value);
+
+  return {
+    getUsersData,
+    getUsersLoading,
+    tempData,
+    loading,
+    refreshData,
+  };
+});
