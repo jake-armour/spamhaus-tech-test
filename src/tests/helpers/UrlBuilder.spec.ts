79a855e29052213a0d884b5cb917fdf60be8810a
Jake Armour <github@jakearmour.com>
2023-07-20 18:42:24 +0100

    test: upload first test to allow codecov to work

---
+++ src/tests/helpers/UrlBuilder.spec.ts
@@ -0,0 +1,25 @@
+import { useBaseUrl } from "@/helpers/UrlBuilder";
+import Constants from "@/state/Constants";
+import { expect, test } from "vitest";
+import { setActivePinia, createPinia } from "pinia";
+import { createTestingPinia } from "@pinia/testing";
+import { UseMainStore } from "@/state/MainStore";
+import { createApp, unref } from "vue";
+
+const testPinia = createTestingPinia();
+
+const app = createApp({});
+const pinia = createPinia();
+app.use(pinia);
+const mainStore = UseMainStore(testPinia);
+
+beforeEach(() => {
+  setActivePinia(pinia);
+});
+
+test("creating url", () => {
+  const url = useBaseUrl("users");
+  const expectedUrl = new URL(`${Constants.API_BASE_URL}/users`);
+
+  expect(unref(url)).toEqual(expectedUrl);
+});
