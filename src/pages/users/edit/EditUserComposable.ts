71f193af543d35cb5c8b8572f2f7a85f2a1fef23
Jake Armour <github@jakearmour.com>
2023-07-20 19:17:28 +0100

    feat(users/edit): add composable to handle loading individual users information

---
+++ src/pages/users/edit/EditUserComposable.ts
@@ -0,0 +1,29 @@
+import { IUserData, IEditAndCreateData } from "@/typings/pages/UserTypes";
+import { useAsyncData } from "@/helpers/DataFetching";
+import { useEditUrl } from "@/helpers/UrlBuilder";
+import { computed } from "vue";
+
+export const useEditUserComposable = () => {
+  const url = useEditUrl("users");
+  const { data, loading } = useAsyncData<IUserData>(url);
+
+  const getEditUserData = computed(() => {
+    return data.value;
+  });
+  const getEditUsersLoading = computed(() => loading.value);
+
+  const getFormattedEditUsersData = computed((): IEditAndCreateData => {
+    if (!getEditUserData.value) return null;
+
+    return {
+      name: getEditUserData.value.name,
+      cc: getEditUserData.value.cc,
+    };
+  });
+
+  return {
+    getEditUserData,
+    getEditUsersLoading,
+    getFormattedEditUsersData,
+  };
+};
