43aa276ac1133566715abec110278faeef7efd24
Jake Armour <github@jakearmour.com>
2023-07-20 19:00:55 +0100

    feat(Users): add table controls for link to create user

---
+++ src/pages/users/UsersTableControls.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="d-flex justify-space-between mb-4">
+    <p>Users</p>
+    <div class="gap-2">
+      <VBtn tag="router-link" to="/users/create" type="filled"
+        >Create User</VBtn
+      >
+    </div>
+  </div>
+</template>
