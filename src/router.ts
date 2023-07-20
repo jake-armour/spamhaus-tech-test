5cf54762c0a65ea94ba554ef2ae41add3ec958bb
Jake Armour <github@jakearmour.com>
2023-07-20 17:59:10 +0100

    feat(router): adding countries route

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,12 @@ import {
 } from "vue-router";
 
 import Users from "@/pages/users/Index.vue";
+import Countries from "@/pages/countries/Index.vue";
 
 const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/users" },
   { path: "/users", component: Users },
+  { path: "/countries", component: Countries },
 ];
 
 const router = createRouter({
