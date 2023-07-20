a217f4ed702bbb85d60c326996ff5b0a9e6a09a2
Jake Armour <github@jakearmour.com>
2023-07-20 19:17:05 +0100

    feat(UrlBuilder): add edit URL using route params

--- src/helpers/UrlBuilder.ts
+++ src/helpers/UrlBuilder.ts
@@ -1,11 +1,28 @@
 import { UseMainStore } from "@/state/MainStore";
 import { storeToRefs } from "pinia";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import { useRoute } from "vue-router";
 
 export const useBaseUrl = (apiType: string) => {
   const { getApiBaseUrl } = storeToRefs(UseMainStore());
 
   return computed(() => {
     return new URL(`${getApiBaseUrl.value}/${apiType}`);
+  });
+};
+
+export const useEditUrl = (apiType: string) => {
+  const route = useRoute();
+  const idValue = ref("");
+
+  if (route?.params.user) {
+    idValue.value = `${route.params.user as string}`;
+  }
+
+  const baseUrl = useBaseUrl(apiType);
+  return computed(() => {
+    const url = new URL(baseUrl.value);
+    url.pathname += `${idValue.value}`;
+    return url;
   });
 };
