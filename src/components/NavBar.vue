075d5bb139761bede91e2873deb7404e9c98964a
Jake Armour <github@jakearmour.com>
2023-07-20 17:58:58 +0100

    feat(NavBar): add content to NavBar, including router links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,8 @@
 <template>
-  <VAppBar></VAppBar>
+  <VAppBar class="mb-12">
+    <VAppBarTitle>SpamHaus Tech Test</VAppBarTitle>
+    <VSpacer />
+    <VBtn :to="{ path: '/users' }">Users</VBtn>
+    <VBtn :to="{ path: '/countries' }">Countries</VBtn>
+  </VAppBar>
 </template>
