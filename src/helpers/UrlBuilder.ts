1f33eeaa312f994d1003af98a21024a9e8c30d8a
Jake Armour <github@jakearmour.com>
2023-07-20 18:44:08 +0100

    feat(UrlBuilder): add helper to build url depending on endpoint

---
+++ src/helpers/UrlBuilder.ts
@@ -0,0 +1,11 @@
+import { UseMainStore } from "@/state/MainStore";
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+
+export const useBaseUrl = (apiType: string) => {
+  const { getApiBaseUrl } = storeToRefs(UseMainStore());
+
+  return computed(() => {
+    return new URL(`${getApiBaseUrl.value}/${apiType}/`);
+  });
+};
