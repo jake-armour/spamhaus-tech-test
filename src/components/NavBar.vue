72cb97ca272f4c5a74a491f850dcfbe2afa78072
Jake Armour <github@jakearmour.com>
2023-07-20 21:17:22 +0100

    style(navbar): added colour and logo to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,15 @@
 <template>
-  <VAppBar class="mb-12">
-    <VAppBarTitle>SpamHaus Tech Test</VAppBarTitle>
+  <VAppBar color="grey-lighten-2" class="mb-12">
+    <VAppBarTitle
+      ><RouterLink :to="{ path: '/users' }"
+        ><img :src="logo" height="50" /></RouterLink
+    ></VAppBarTitle>
     <VSpacer />
     <VBtn :to="{ path: '/users' }">Users</VBtn>
     <VBtn :to="{ path: '/countries' }">Countries</VBtn>
   </VAppBar>
 </template>
+
+<script setup lang="ts">
+import logo from "@/assets/logo.png";
+</script>
