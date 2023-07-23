086e7f490876a745239eb5ca425c0650c6ab9ede
Jake Armour <github@jakearmour.com>
2023-07-23 18:01:42 +0200

    feat: create reusable dialog for creating and editing user

---
+++ src/components/EditCreateDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <VDialog max-width="500" v-model="props.value">
+    <VCard :prepend-icon="props.icon">
+      <template v-slot:title>{{ props.title }}</template>
+      <VCardText>{{ props.text }}</VCardText>
+      <VCardActions>
+        <VBtn color="blue-lighten-4" @click="emit('close')">Close</VBtn>
+      </VCardActions>
+    </VCard>
+  </VDialog>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["close"]);
+
+const props = defineProps({
+  value: Boolean,
+  title: String,
+  text: String,
+  icon: String,
+});
+</script>
